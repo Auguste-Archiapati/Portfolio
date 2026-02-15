ef4557e0a04a46cf8f464af7f9aea1beddac6862
Auguste Archiapati <archiagust@gmail.com>
2026-02-15 22:31:37 +0100

    second

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,6 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({});
-
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://auguste-archiapati.github.io',
+  site: 'https://Auguste-Archiapati.github.io',
   base: '/Portfolio',
 });
